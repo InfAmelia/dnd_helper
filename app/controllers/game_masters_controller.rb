0377240e58fe5cff32869bab124e9756a0c6a179
byendan <byendanb@gmail.com>
2016-07-20 16:40:10 -0700

    Changed the active gm into a session variable. Can create a new one, and log out

--- app/controllers/game_masters_controller.rb
+++ app/controllers/game_masters_controller.rb
@@ -1,6 +1,6 @@
 class GameMastersController < ApplicationController
   def show
-    @game_master = GameMaster.find(params[:id])
+    @game_master = current_gm
     @players = @game_master.party.players
   end
 
@@ -13,15 +13,26 @@ class GameMastersController < ApplicationController
     @game_master = GameMaster.new(params[:game_master])
     @party = Party.new
 
+    # Populates the party with the members
+    # who were checked off
     fill_party(params[:party])
 
     @game_master.party = @party
     if @game_master.save
+      session[:current_gm] = @game_master.id
       redirect_to @game_master
     else
       render action: "new"
     end
 
+  end
+
+  def destroy
+    @game_master = current_gm
+    end_session
+    @game_master.destroy
+
+    redirect_to root_path
   end
 
   def fill_party(party_hash)
